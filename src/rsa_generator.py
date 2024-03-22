aa28ff2a8ef505f732c98b870d3221cce9f9fa87
lukrzak <lukrzak@wp.pl>
2024-03-22 13:07:33 +0100

    Code refactor

--- src/rsa_generator.py
+++ src/rsa_generator.py
@@ -3,10 +3,13 @@ import sys
 
 
 def generate_keys(private_key_save_path: str, public_key_save_path: str, pin: str) -> None:
-    RSA_ITERATIONS: int = 21000     # Recommended value
-    SALT_SIZE = 8
-    BLOCK_SIZE = 8
-    KEY_LENGTH = 4096
+    RSA_ITERATIONS: int = 21_000     # Recommended value
+    SALT_SIZE: int = 8
+    BLOCK_SIZE: int = 8
+    KEY_LENGTH: int = 4_096
+    HASHING_ALGORITHM: str = 'SHA512'
+    ENCRYPTION_ALGORITHM: str = 'AES256-CBC'
+    PROTECTION_ALGORITHMS: str = f'PBKDF2WithHMAC-{HASHING_ALGORITHM}And{ENCRYPTION_ALGORITHM}'
 
     key = RSA.generate(KEY_LENGTH)
     with open(private_key_save_path + "/private_key.pem", "wb") as f:
@@ -20,7 +23,7 @@ def generate_keys(private_key_save_path: str, public_key_save_path: str, pin: st
         }
         data = key.export_key(passphrase=pin,
                               pkcs=8,
-                              protection='PBKDF2WithHMAC-SHA512AndAES256-CBC',
+                              protection=PROTECTION_ALGORITHMS,
                               prot_params=protection_settings)
         f.write(data)
 
@@ -31,7 +34,7 @@ def generate_keys(private_key_save_path: str, public_key_save_path: str, pin: st
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        raise Exception("Expected arguments: \n- Private key path\n- Public key path\n- PIN value")
+        raise Exception("Expected arguments: \n- Private key save path\n- Public key save path\n- PIN value")
 
     private_key_path: str = sys.argv[1]
     public_key_path: str = sys.argv[2]
