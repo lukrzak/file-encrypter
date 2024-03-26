a17c80715f6d4efb5185e16739075e696e3b8afb
KacperPuda <125309126+KacperPuda@users.noreply.github.com>
2024-03-27 00:42:22 +0100

    GUI continue

--- src/app.py
+++ src/app.py
@@ -1,42 +1,74 @@
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import ttk  # Import ttk for styled widgets
+from customtkinter import *
+import customtkinter
+from rsa_generator import generate_keys
 
 import os
 import time
 
+customtkinter.set_default_color_theme("src/themes/light.json")
+
 def browse_file():
-    global filenames
     filetypes = (
         ("PDF", "*.pdf"),
         ("C++", "*.cpp")
     )
     filenames = filedialog.askopenfilenames(filetypes=filetypes)
-    selected_files_label.config(text=", ".join(os.path.basename(file) for file in filenames))
+    selected_files_label.configure(text=", ".join(os.path.basename(file) for file in filenames))
 
 def encrypting_clicked():
-    global filenames
     # Encrypting logic
+    disc = filedialog.askdirectory(initialdir="/", title="Select disk")
+    print("XDDDDDDDDD")
+    print(disc)
+    priv_key_path = disc + "fileEncrypter"
+    print("XDDDDDDDDD22222222")
+    print(priv_key_path)
+    if not os.path.exists(priv_key_path):
+        os.makedirs(priv_key_path)
+    pub_key_path = "./publicKeys"
+    pin = pin_window()
+    generate_keys(priv_key_path, pub_key_path, pin)
     perform_operation("Encrypting")
-   
+
 def perform_operation(operation):
-    global filenames   
-    progress_label.pack()
-    progress_bar.pack()
-    # Simulate operation
-    progress_label.config(text="Operation in progress...")
+    progress_bar.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+    progress_bar.set(0)
+    progress_label.configure(text="Operation in progress...")
     for i in range(100):
         time.sleep(0.02)  # Simulate long operation
-        progress_bar["value"] = i
-        root.update_idletasks()  # Update interface
-    progress_label.config(text="Operation completed.")
+        progress_bar.set(i/100)
+        app.update_idletasks()  # Update interface
+    progress_label.configure(text="Operation completed.")
+    progress_bar.grid_forget()
+    progress_label.grid_forget()
     time.sleep(1)
-    progress_label.forget()
-    progress_bar.forget()
+    app.update_idletasks()
+
+def pin_window():
+    pin_window = CTkToplevel(app)
+    pin_window.title("Enter PIN")
+
+    def enter_pressed(event):
+        entered_pin = pin_entry.get()
+        return entered_pin
+
+    pin_label = CTkLabel(pin_window, text="Enter PIN:")
+    pin_label.pack()
+
+    pin_entry = CTkEntry(pin_window, show="*")
+    pin_entry.pack()
+
+    # Label to display incorrect PIN message
+    pin_error_label = CTkLabel(pin_window, text="")
+    pin_error_label.pack()
+
+    # Handle Enter key press
+    pin_entry.bind("<Return>", enter_pressed)
+    
 
 def decrypting_clicked():
     # Create new window for entering PIN
-    pin_window = tk.Toplevel(root)
+    pin_window = CTkToplevel(app)
     pin_window.title("Enter PIN")
 
     def enter_pressed(event):
@@ -47,64 +79,50 @@ def decrypting_clicked():
             perform_operation("Decrypting")
         else:
             # Incorrect PIN message
-            pin_error_label.config(text="Incorrect PIN!")
+            pin_error_label.configure(text="Incorrect PIN!")
 
-    pin_label = ttk.Label(pin_window, text="Enter PIN:")
+    pin_label = CTkLabel(pin_window, text="Enter PIN:")
     pin_label.pack()
 
-    pin_entry = ttk.Entry(pin_window, show="*")
+    pin_entry = CTkEntry(pin_window, show="*")
     pin_entry.pack()
 
     # Label to display incorrect PIN message
-    pin_error_label = ttk.Label(pin_window, text="", foreground="red")
+    pin_error_label = CTkLabel(pin_window, text="")
     pin_error_label.pack()
 
     # Handle Enter key press
     pin_entry.bind("<Return>", enter_pressed)
 
-def on_usb_insert(event):
-    global filenames
-    print("USB device inserted:", event.device_node)
-
 # Create main window
-root = tk.Tk()
-root.title("File Encrypter")
-root.geometry("300x150")
+app = CTk()
+app.title("File Encrypter")
+app.geometry("300x200")
 
 filenames = ""
 
-# Create button style using ttk
-style = ttk.Style()
-style.configure('TButton', font=('calibri', 10, 'bold'), borderwidth='4')
-
 # File browsing button
-browse_button = ttk.Button(root, text="Files", command=browse_file, style='TButton')
-browse_button.pack()
+browse_button = CTkButton(app, text="Files", command=browse_file, border_width=2)
+browse_button.grid(row=0, column=0, padx=5, pady=5, columnspan=2)
 
 # Label for selected files
-selected_files_label = ttk.Label(root, text="Selected file(s)")
-selected_files_label.pack()
+selected_files_label = CTkLabel(app, text="Selected file(s)")
+selected_files_label.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
 # Decrypting button
-decryptingButton = ttk.Button(root, text="Decrypting", command=decrypting_clicked, style='TButton')
-decryptingButton.pack()
+decryptingButton = CTkButton(app, text="Decrypting", command=decrypting_clicked, border_width=2)
+decryptingButton.grid(row=2, column=1, padx=5, pady=5)
 
 # Encrypting button
-encryptingButton = ttk.Button(root, text="Encrypting", command=encrypting_clicked, style='TButton')
-encryptingButton.pack()
+encryptingButton = CTkButton(app, text="Encrypting", command=encrypting_clicked, border_width=2)
+encryptingButton.grid(row=2, column=0, padx=5, pady=5)
 
 # Progress bar (progress bar)
-progress_frame = ttk.Frame(root)
-progress_frame.pack(pady=10)
+progress_label = CTkLabel(app, text="")
+progress_label.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
-progress_label = ttk.Label(progress_frame, text="")
-progress_bar = ttk.Progressbar(progress_frame, orient="horizontal", length=250, mode="determinate")
+progress_bar = CTkProgressBar(app, orientation='horizontal')
+
 
 # Run main loop
-root.mainloop()
-
-context = pyudev.Context()
-monitor = pyudev.Monitor.from_netlink(context)
-monitor.filter_by(subsystem='usb')
-observer = pyudev.MonitorObserver(monitor, on_usb_insert)
-observer.start()
+app.mainloop()
