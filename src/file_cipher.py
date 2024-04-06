cd00f82f545d077bedebec5ddbbf282b4e054f52
lukrzak <lukrzak@wp.pl>
2024-04-06 15:39:33 +0200

    Implementation of the XAdES signature and verification

--- src/file_cipher.py
+++ src/file_cipher.py
@@ -1,6 +1,12 @@
+from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.x509 import load_der_x509_certificate
+from lxml import etree
+import datetime
+import base64
+import os
 
 
 SYMMETRIC_KEY_LENGTH: int = 256
@@ -28,6 +34,7 @@ def encrypt_file(file_path: str, public_key_path: str, output_path: str, output_
         content,
         padding.PKCS1v15()
     )
+
     return get_content(output_mode, encrypted_text, output_path)
 
 
@@ -44,6 +51,7 @@ def decrypt_key(file_path: str, private_key_path: str, pin: str, output_path: st
         content,
         padding.PKCS1v15()
     )
+
     return get_content(output_mode, decrypted_text, output_path)
 
 
@@ -68,3 +76,97 @@ def get_content(output_mode: str, content: bytes, output_path: str):
         return f"Saved to file {output_path}"
     else:
         raise Exception("Unknown output mode. Allowed values are 'file' or 'value'")
+
+
+def generate_file_signature(file_path: str, private_key_path: str, certificate_path: str, pin: str, signature_output_path: str):
+    XADES_TEPMLATE_PATH: str = "xades_teamplate.xml"
+    with open(private_key_path, "rb") as f:
+        private_key = serialization.load_pem_private_key(
+            f.read(),
+            password=load_pin_hash(pin)
+        )
+    with open(file_path, "rb") as f:
+        content_to_sign = f.read()
+
+    signature_value: str = get_file_signature(private_key, content_to_sign)
+    file_hash: str = get_file_hash(content_to_sign)
+    certificate: str = get_certificate_value(certificate_path)
+
+    with open(XADES_TEPMLATE_PATH, "r") as f:
+        root = etree.fromstring(f.read())
+    replace_xades_template_placeholders(root, signature_value, file_hash, certificate, file_path)
+
+    with open(signature_output_path, "wb") as f:
+        f.write(etree.tostring(root, encoding="utf-8"))
+
+
+def get_file_signature(private_key, content: bytes):
+    signature = private_key.sign(
+        content,
+        padding.PKCS1v15(),
+        hashes.SHA256()
+    )
+
+    return base64.b64encode(signature).decode('utf-8')
+
+
+def get_file_hash(content: bytes):
+    hasher = hashes.Hash(hashes.SHA256())
+    hasher.update(content)
+    hash_value = hasher.finalize()
+
+    return base64.b64encode(hash_value).decode('utf-8')
+
+
+def get_certificate_value(certificate_path: str):
+    with open(certificate_path, "rb") as f:
+        cert = f.read()
+
+    return base64.b64encode(cert).decode('utf-8')
+
+
+def replace_xades_template_placeholders(root, signature_value: str, file_hash: str, certificate: str, file_path: str):
+    signature_value_element = root.find(".//{http://www.w3.org/2000/09/xmldsig#}SignatureValue")
+    digest_element = root.find(".//{http://www.w3.org/2000/09/xmldsig#}DigestValue")
+    certificate_element = root.find(".//{http://www.w3.org/2000/09/xmldsig#}X509Certificate")
+    signature_timestamp_element = root.find(".//SignedSignatureProperties")
+    object_metadata_element = root.find(".//SignedDataObjectProperties")
+
+    file_size = os.path.getsize(file_path)
+    modification_time = os.path.getmtime(file_path)
+    file_format = os.path.splitext(file_path)[1]
+
+    signature_value_element.text = signature_value
+    digest_element.text = file_hash
+    certificate_element.text = certificate
+    signature_timestamp_element.text = str(datetime.datetime.utcnow())
+    object_metadata_element.text = f"{file_size}B,{modification_time},{file_format}"
+
+
+def verify_signature(file_path: str, xades_signature_path: str):
+    with open(xades_signature_path, "r") as f:
+        root = etree.fromstring(f.read())
+    with open(file_path, "rb") as f:
+        content = f.read()
+
+    signature = base64.b64decode(root.find(".//{http://www.w3.org/2000/09/xmldsig#}SignatureValue").text.encode())
+    # TODO compare hash result with digest value
+    digest = base64.b64decode(root.find(".//{http://www.w3.org/2000/09/xmldsig#}DigestValue").text.encode())
+    certificate_value = base64.b64decode(root.find(".//{http://www.w3.org/2000/09/xmldsig#}X509Certificate").text.encode())
+    certificate = load_der_x509_certificate(certificate_value)
+    public_key = certificate.public_key()
+
+    try:
+        public_key.verify(
+            signature,
+            content,
+            padding.PKCS1v15(),
+            hashes.SHA256()
+        )
+        print("kox")
+    except InvalidSignature:
+        print("Invalid")
+
+
+# generate_file_signature("test.txt", "private.pem", "certificate.der", "1234", "outaa.xml")
+# verify_signature("test.txt", "outaa.xml")
